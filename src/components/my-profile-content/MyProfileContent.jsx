3553d5404e156134409681cee0344afe89dc6faf
Dwi M Nurafli <dwirafli13@gmail.com>
2023-10-28 14:51:19 +0700

    add modal preview post at profile and user page

--- src/components/my-profile-content/MyProfileContent.jsx
+++ src/components/my-profile-content/MyProfileContent.jsx
@@ -3,10 +3,11 @@ import { useParams } from "react-router";
 import axios from "axios";
 import "./MyProfileContent.css";
 import useLoggedUser from "../../hooks/useLoggedUser";
+import useExplore from "../../hooks/useExplore";
 
 const MyProfileContent = () => {
   const { loggedUser } = useLoggedUser();
-
+  const { handleUser, isLike } = useExplore();
   const [myProfilePost, setMyProfilePost] = useState([]);
   const [totalPost, setTotalPost] = useState("");
   const param = useParams();
@@ -81,15 +82,107 @@ const MyProfileContent = () => {
         </p>
         <p>{loggedUser?.bio}</p>
         <p>
-          <a href={loggedUser?.website} target="__blank">{loggedUser?.website}</a>
+          <a href={loggedUser?.website} target="__blank">
+            {loggedUser?.website}
+          </a>
         </p>
       </div>
       <div className="row">
         {myProfilePost.map((item, key) => (
           <div key={key} className="col-4 px-1 py-1">
+            {/* Modal Button Trigger */}
             <div className="image-responsive">
-              <img src={item.imageUrl} width={200} height={200} />
+              <button
+                className="btn card-text p-0 ms-2"
+                data-bs-toggle="modal"
+                data-bs-target={`#postModal${item?.id}`}
+              >
+                <img src={item.imageUrl} width={200} height={200} />
+              </button>
             </div>
+            {/* Modal Button Trigger */}
+
+            {/* Modal Content */}
+            <div
+              className="modal fade"
+              id={`postModal${item?.id}`}
+              data-bs-backdrop="static"
+              data-bs-keyboard="false"
+              tabindex="-1"
+              aria-labelledby="staticBackdropLabel"
+              aria-hidden="true"
+            >
+              <div className="modal-dialog modal-dialog-centered modal-xl modal-fullscreen-sm-down">
+                <div className="modal-content">
+                  <div className="modal-header">
+                    <button
+                      className="d-flex align-items-center btn p-0"
+                      onClick={() => handleUser(item?.userId)}
+                    >
+                      <img
+                        src={item?.user?.profilePictureUrl}
+                        className="rounded-circle photo-profile"
+                        width={40}
+                        height={40}
+                      />
+                      <p
+                        className="modal-title fs-5 ms-2"
+                        id="staticBackdropLabel"
+                      >
+                        {item?.user?.username}
+                      </p>
+                    </button>
+                    <button
+                      type="button"
+                      className="btn-close"
+                      data-bs-dismiss="modal"
+                      aria-label="Close"
+                    ></button>
+                  </div>
+                  <div className="modal-body">
+                    <div className="card">
+                      <img
+                        src={item?.imageUrl}
+                        className="card-img-top img-fluid"
+                      />
+                      <div className="card-body">
+                        <div className="btn-group">
+                          {isLike ? (
+                            <button
+                              className="btn card-text p-0"
+                              onClick={() => handleUnlikePost(item?.id)}
+                            >
+                              Unlike
+                            </button>
+                          ) : (
+                            <button
+                              className="btn card-text p-0"
+                              onClick={() => handleLikePost(item?.id)}
+                            >
+                              Like
+                            </button>
+                          )}
+                          <button className="btn card-text p-0 ms-2">
+                            Comment
+                          </button>
+                        </div>
+                        <p className="card-text">{item?.totalLikes} Likes</p>
+                        <p className="card-text">
+                          <button
+                            className="btn p-0"
+                            onClick={() => handleUser(item?.userId)}
+                          >
+                            <strong>{item?.user?.username}</strong>
+                          </button>{" "}
+                          {item?.caption}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            {/* Modal Content */}
           </div>
         ))}
       </div>
--- src/components/user-content/UserContent.jsx
+++ src/components/user-content/UserContent.jsx
@@ -2,11 +2,13 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import useLoggedUser from "../../hooks/useLoggedUser";
 import useFollow from "../../hooks/useFollow";
+import useExplore from "../../hooks/useExplore";
 import axios from "axios";
 
 const UserContent = () => {
   const { loggedUser } = useLoggedUser();
   const { isFollow, handleFollow, handleUnfollow } = useFollow();
+  const { handleUser, isLike } = useExplore();
   const [userPost, setUserPost] = useState([]);
   const [userById, setUserById] = useState({});
   const [totalPost, setTotalPost] = useState("");
@@ -133,9 +135,99 @@ const UserContent = () => {
       <div className="row">
         {userPost.map((item, key) => (
           <div key={key} className="col-4 px-1 py-1">
+            {/* Modal Button Trigger */}
             <div className="image-responsive">
-              <img src={item.imageUrl} width={200} height={200} />
+              <button
+                className="btn card-text p-0 ms-2"
+                data-bs-toggle="modal"
+                data-bs-target={`#postModal${item?.id}`}
+              >
+                <img src={item.imageUrl} width={200} height={200} />
+              </button>
             </div>
+            {/* Modal Button Trigger */}
+
+            {/* Modal Content */}
+            <div
+              className="modal fade"
+              id={`postModal${item?.id}`}
+              data-bs-backdrop="static"
+              data-bs-keyboard="false"
+              tabindex="-1"
+              aria-labelledby="staticBackdropLabel"
+              aria-hidden="true"
+            >
+              <div className="modal-dialog modal-dialog-centered modal-xl modal-fullscreen-sm-down">
+                <div className="modal-content">
+                  <div className="modal-header">
+                    <button
+                      className="d-flex align-items-center btn p-0"
+                      onClick={() => handleUser(item?.userId)}
+                    >
+                      <img
+                        src={item?.user?.profilePictureUrl}
+                        className="rounded-circle photo-profile"
+                        width={40}
+                        height={40}
+                      />
+                      <p
+                        className="modal-title fs-5 ms-2"
+                        id="staticBackdropLabel"
+                      >
+                        {item?.user?.username}
+                      </p>
+                    </button>
+                    <button
+                      type="button"
+                      className="btn-close"
+                      data-bs-dismiss="modal"
+                      aria-label="Close"
+                    ></button>
+                  </div>
+                  <div className="modal-body">
+                    <div className="card">
+                      <img
+                        src={item?.imageUrl}
+                        className="card-img-top img-fluid"
+                      />
+                      <div className="card-body">
+                        <div className="btn-group">
+                          {isLike ? (
+                            <button
+                              className="btn card-text p-0"
+                              onClick={() => handleUnlikePost(item?.id)}
+                            >
+                              Unlike
+                            </button>
+                          ) : (
+                            <button
+                              className="btn card-text p-0"
+                              onClick={() => handleLikePost(item?.id)}
+                            >
+                              Like
+                            </button>
+                          )}
+                          <button className="btn card-text p-0 ms-2">
+                            Comment
+                          </button>
+                        </div>
+                        <p className="card-text">{item?.totalLikes} Likes</p>
+                        <p className="card-text">
+                          <button
+                            className="btn p-0"
+                            onClick={() => handleUser(item?.userId)}
+                          >
+                            <strong>{item?.user?.username}</strong>
+                          </button>{" "}
+                          {item?.caption}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            {/* Modal Content */}
           </div>
         ))}
       </div>
